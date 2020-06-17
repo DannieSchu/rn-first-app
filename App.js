de3b5df5e5ffad4715aad5d6db156ec48413d295
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-17 00:55:04 -0700

    set up Core Components and associated StyleSheet

--- App.js
+++ App.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="First Goal" style={styles.input} />
+        <View style={styles.buttonContainer}>
+          <Button title="Add" color="deeppink" />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    borderBottomColor: 'darkslategrey', 
+    borderBottomWidth: 0.5, 
+    color: 'darkslategrey', 
+    flex: 2
+  },
+  buttonContainer: {
+    flex: 1
+  }
+})
