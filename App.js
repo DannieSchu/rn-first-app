3e03ef4653d8966a3246ae6d47dc177375a9f8ad
Dannie Schumaker <draeschumaker@gmail.com>
2020-06-17 01:36:23 -0700

    render list with ScrollView

--- App.js
+++ App.js
@@ -1,15 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [goalList, setGoalList] = useState([]);
+
+  const inputHandler = enteredText => {
+    setEnteredGoal(enteredText);
+  };
+
+  const buttonHandler = () => {
+    setGoalList(currentGoals => ([enteredGoal, ...currentGoals]))
+  };
+
+  const listElements = goalList.map((goal, i) => (
+    <View key={i} style={styles.listItem}>
+      <Text style={styles.text}>{goal}</Text>
+    </View>
+  ));
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="First Goal" style={styles.input} />
+        <TextInput
+          placeholder="First Goal"
+          style={styles.input}
+          onChangeText={inputHandler}
+          value={enteredGoal}
+        />
         <View style={styles.buttonContainer}>
-          <Button title="Add" color="deeppink" />
+          <Button 
+            title="Add" 
+            color="deeppink" 
+            onPress={buttonHandler}
+          />
         </View>
       </View>
+      <ScrollView>
+        {listElements}
+      </ScrollView>
     </View>
   );
 }
@@ -24,12 +53,21 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   input: {
-    borderBottomColor: 'darkslategrey', 
-    borderBottomWidth: 0.5, 
-    color: 'darkslategrey', 
+    borderBottomColor: 'darkslategrey',
+    borderBottomWidth: 0.5,
+    color: 'darkslategrey',
     flex: 2
   },
   buttonContainer: {
     flex: 1
+  },
+  listItem: {
+    backgroundColor: 'darkseagreen',
+    padding: 10,
+    marginTop: 15,
+    borderRadius: 8
+  },
+  text: {
+    color: 'white'
   }
 })
